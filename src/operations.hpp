c6070942c4bc467d3596ad8947a0c04e1257a784
jachan <johnachanya9@gmail.com>
2023-04-25 14:36:02 +0200

    starting a new class design for operations

---
+++ src/operations.hpp
@@ -0,0 +1,11 @@
+#ifndef _OPERATIONS_H_
+#define _OPERATIONS_H_
+
+/* I realize I should start writing comments and descriptions
+    The operation class perform linear algera "Operations" and visualize it
+*/
+
+class Operations
+{};
+
+#endif
