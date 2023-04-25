09960c072d625b546317fbf3bf73df5d5ad7a347
jachan <johnachanya9@gmail.com>
2023-04-25 14:41:19 +0200

    added the interface for our operation class

--- src/operations.hpp
+++ src/operations.hpp
@@ -5,7 +5,15 @@
     The operation class perform linear algera "Operations" and visualize it
 */
 
+#include "matrix_object.hpp"
+#include "feature_object.hpp"
+
 class Operations
-{};
+{
+public:
+    static void matmul(MatrixObject& mat1, MatrixObject& mat2);
+    static void dot(FeatureObj& vec1, FeatureObj& vec2);
+    static void scalarVecMul(FeatureObj& vec, float scale);
+};
 
 #endif
